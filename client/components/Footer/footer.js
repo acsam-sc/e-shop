d2611a0f1fd296053c78bfebb7030c64c9183726
Anton <acsam42@gmail.com>
2020-11-29 02:06:36 +0100

    fix total.toFixed(2)

--- client/components/Footer/footer.js
+++ client/components/Footer/footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Footer = (props) => {
+  const total = props.itemsInCartArray.reduce((acc, item) => {
+    const summ = acc
+    return summ + item.price * item.amount * props.currencyCoefficient
+  }, 0).toFixed(2)
+
   return (
     <div className="flex fixed bottom-0 w-full h-12 justify-end md:px-24 font-bold md:text-2xl bg-white border-t-2 items-center">
       Order total:{' '}
-      {props.itemsInCartArray.reduce((acc, item) => {
-        const total = acc
-        return total + item.price * item.amount * props.currencyCoefficient
-      }, 0)}{' '}
+      {total}{' '}
       {props.currency}
     </div>
   )
